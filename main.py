4f20be226302ca065df8a9a77764a2aae91caabc
Christian Alleyne <chrisalleyne3@gmail.com>
2024-12-18 10:50:22 -0500

    woooooo

--- main.py
+++ main.py
@@ -2,11 +2,45 @@ from flask import Flask, render_template
 import pymysql
 from dynaconf import Dynaconf
 from flask import request
+import flask_login
 app = Flask(__name__)
 
 conf = Dynaconf(
     settings_file = ["settings.toml" ]
+
 )
+app = Flask(__name__)
+app.secrect_key = conf.secret_key
+
+login_manager = flask_login.LoginManager()
+login_manager.init_app(app)
+
+class User:
+    is_authenticated = True
+    is_anonymous = False
+    is_active = True
+
+    def __init__(self, id, username, email, first_name, last_name):
+        self.id = id
+        self.username = username
+        self.email = email
+        self.first_name = first_name
+        self. last_name = last_name
+    def get_id(self):
+        return str(self.id)
+    
+    def load_user(user_id):
+        conn = connect_db()
+        cursor = conn.cursor()
+        result = cursor.fetchone()
+
+        cursor.close()
+        conn.close()
+
+        return User(result["id"], result["username"], result["email"], result["first_name"], result["last_name"])
+
+
+
 
 def connect_db():
     conn = pymysql.connect(
@@ -84,5 +118,30 @@ def sign_up():
         finally:
             cursor.close()
             conn.close()
+
+@app.route("/signin", methods = ["POST", "GET"])
+def sign_in():
+    username = request.form["Username"].strip
+    password = request.form["Password"]
+
+    conn = connect_db()
+    cursor = conn.cursor()
+
+    cursor.execute(f"SELECT * FROM `Custoner` WHERE `username` = '{username}';")
+
+    result = cursor.fetchone()
+
+    if result is None:
+        flash("your username/password is incorrect")
+    elif password != result["password"]:
+        flash("your username/password is incorrect")
+    else:
+        user = User(result["id"], result["username"], result["email"], result["first_name"], result["last_name"])
         
+ 
     return render_template("sign_up.html.jinja")
+
+@app.route('/logout')
+def logout():
+    flask_login.logout_user()
+    return redirect('/')
